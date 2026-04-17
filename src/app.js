57f87accc2d4438f2edb3334eb4bb01139ba424e
Souvik Ghosh <ghosh2005202318@gmail.com>
2026-04-17 14:30:44 +0530

    init

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+
+const authRoute = require("./routes/auth.route")
+
+const cookieParser = require("cookie-parser")
+
+const app = express()
+app.use(express.urlencoded({ extended: false }))
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("/api/auth", authRoute)
+
+
+module.exports = app
